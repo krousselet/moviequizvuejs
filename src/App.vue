4ba6c60072d30f939bdb310a1236d45039e72794
krousselet <krous74@gmail.com>
2024-01-29 15:27:25 +0100

    Swiper initialized and at last properly rendered

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,26 @@
   <header>
     <Navbar :pages="pages" :activePage="activePage" :dropdowns="dropdowns"></Navbar>
   </header>
+  <main>
+    <Swiper :games="games"></Swiper>
+  </main>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import Navbar from './components/Navbar.vue';
+import Swiper from './components/Swiper.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Swiper,
   },
   data() {
+
     return {
       activePage: 0,
+      //********************** NAVBAR DROPDOWNS ******************************/
       dropdowns: [
         {
           link: {
@@ -24,6 +31,7 @@ export default {
           }
         },
       ],
+      //********************** NAVBAR ******************************/
       pages: [
         {
           link: {
@@ -46,6 +54,56 @@ export default {
             title: 'This link goes to ',
           }
         },
+      ],
+
+      //************************* SWIPER DATA **********************************/
+      games: [
+        {
+          movies: [
+            {
+              url: 'https://media.vanityfair.fr/photos/6426aa2f26a0c074c81a3937/16:9/w_2560%2Cc_limit/GettyImages-156473199.jpg',
+              title: 'Gladiator',
+              description: '',
+            },
+            {
+              url: 'https://movieplayer.net-cdn.it/t/images/2023/05/26/pearl-harbor_jpg_1600x900_crop_q85.jpg',
+              title: 'Pearl Harbor',
+              description: '',
+            },
+            {
+              url: 'https://www.usatoday.com/gcdn/-mm-/2abc4a8ff8153af3327de4720edcf8a76bc28036/c=302-0-1441-856/local/-/media/USATODAY/USATODAY/2014/08/06/1407371057000-XXX-TEENAGE-MUTANT-NINJA-TURTLES-MOV-JY-1478--66316298.JPG',
+              title: 'Ninja Turtles',
+              description: '',
+            },
+            {
+              url: 'https://i0.wp.com/theartofcostume.com/wp-content/uploads/2021/10/nightmare-on-elm-street-movies-in-order-index-1632944846.jpeg?fit=1024%2C514&ssl=1',
+              title: 'Freddy',
+              description: '',
+            },
+          ],
+          series: [
+            {
+              url: 'https://leclaireur.fnac.com/wp-content/uploads/2023/09/totally-spies-1256x720.jpg',
+              title: 'Totaly Spies',
+              description: '',
+            },
+            {
+              url: 'https://i.insider.com/5956abe5b4db9bea5b8b4968?width=700',
+              title: 'Charmed',
+              description: '',
+            },
+            {
+              url: 'https://media.wired.com/photos/5955beec63d43b038e9bc7be/master/pass/Agents-of-SHIELD.jpg',
+              title: 'Agents of Shield',
+              description: '',
+            },
+            {
+              url: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p11408531_i_v8_aa.jpg',
+              title: 'Daredevil',
+              description: '',
+            },
+          ],
+        }
       ]
     }
   },
---
+++ src/components/Swiper.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="(movie, movieIndex) in allMovies" :key="movieIndex">
+                <img :src="movie.url" :alt="movie.title">
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-scrollbar"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['games'],
+    computed: {
+        allMovies() {
+            return this.games.flatMap(game => game.movies);
+        },
+    },
+    mounted() {
+        this.swiper = new Swiper('.swiper', {
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 1,
+            breakpoints: {
+                580: {
+                    slidesPerView: 3
+                },
+                992: {
+                    slidesPerView: 6
+                }
+            },
+
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        })
+    },
+}
+</script>
+
+<style scoped>
+.test {
+    background-size: cover;
+    background-position: center;
+}
+
+img {
+    max-height: 250px;
+    max-height: 325px;
+}
+</style>
